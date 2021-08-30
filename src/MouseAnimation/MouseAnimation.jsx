ed082752375a91a6f92f1137b27925b7f49c17d7
Rabee Aljaramani <77026472+rabeexxxx@users.noreply.github.com>
2021-08-30 15:20:44 +0400

    ewd

--- src/MouseAnimation/MouseAnimation.jsx
+++ src/MouseAnimation/MouseAnimation.jsx
@@ -5,9 +5,10 @@ const MouseAnimation = () => {
    const {season_name,season_img} = useSelector(state=>state.Season)
     useEffect(() => {
         let root=document.documentElement
+        var width=window.innerWidth
         root.addEventListener('mousemove',e=>{
             // console.log(e.clientY)
-
+            if(width>1800){
             gsap.to('.mouse-pointer',{
                 scale:0.8,
                 skewX:-10,
@@ -15,8 +16,58 @@ const MouseAnimation = () => {
                 y:e.clientY-750,
                 stagger:0.1,
                 display:'block'
-
             })
+        }
+        if(width>1600 && width<1800){
+            gsap.to('.mouse-pointer',{
+                scale:0.8,
+                skewX:-10,
+                x:e.clientX,
+                y:e.clientY-780,
+                stagger:0.1,
+                display:'block'
+            })
+        }
+        if(width>1400 && width<1600){
+            gsap.to('.mouse-pointer',{
+                scale:0.8,
+                skewX:-10,
+                x:e.clientX,
+                y:e.clientY-830,
+                stagger:0.1,
+                display:'block'
+            })
+        }
+        if(width>1150 && width<1400){
+            gsap.to('.mouse-pointer',{
+                scale:0.8,
+                skewX:-10,
+                x:e.clientX,
+                y:e.clientY-880,
+                stagger:0.1,
+                display:'block'
+            })
+        }
+        if(width>800 && width<1150){
+            gsap.to('.mouse-pointer',{
+                scale:0.8,
+                skewX:-10,
+                x:e.clientX,
+                y:e.clientY-500,
+                stagger:0.1,
+                display:'block'
+            })
+        }
+        else{
+            gsap.to('.mouse-pointer',{
+                scale:0.8,
+                skewX:-10,
+                x:e.clientX,
+                y:e.clientY-580,
+                stagger:0.1,
+                display:'block'
+            })
+        }
         })
     }, []);
     return (
