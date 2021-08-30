f8d4047bc84d299284d379dd6cdaa847e1f31fbd
Rabee Aljaramani <77026472+rabeexxxx@users.noreply.github.com>
2021-08-30 15:38:16 +0400

    xex

--- src/MouseAnimation/MouseAnimation.jsx
+++ src/MouseAnimation/MouseAnimation.jsx
@@ -58,7 +58,7 @@ const MouseAnimation = () => {
                 display:'block'
             })
         }
-        else{
+        if(width<800){
             gsap.to('.mouse-pointer',{
                 scale:0.8,
                 skewX:-10,
@@ -81,13 +81,6 @@ const MouseAnimation = () => {
             <img className='pointer7 mouse-pointer' src={season_img} alt={season_name}/>
             <img className='pointer8 mouse-pointer' src={season_img} alt={season_name}/>
             <img className='pointer9 mouse-pointer' src={season_img} alt={season_name}/>
-            {/* <div className='pointer1 mouse-pointer'></div>
-            <div className='pointer2 mouse-pointer'></div>
-            <div className='pointer3 mouse-pointer'></div>
-            <div className='pointer4 mouse-pointer'></div>
-            <div className='pointer5 mouse-pointer'></div>
-            <div className='pointer6 mouse-pointer'></div>
-            <div className='pointer7 mouse-pointer'></div> */}
             
         </div>
     );
