80e5ff6aaf6fcf245f1c707c85c389d1fd9f8725
Rabee Aljaramani <77026472+rabeexxxx@users.noreply.github.com>
2021-09-13 18:09:55 +0400

    Update About.jsx

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -6,12 +6,12 @@ const About = () => {
       <h1 className="title">ABOUT</h1>
       <div className="brief">
         First-rate Informatics Engineer with Innovative tech mind and +9 years
-        of experience in developing, programming and supporting. FrontEnd
+        of experience in developing, programming and supporting. A FrontEnd
         developer with 2 years' experience in React-js, JavaScript, HTML CSS and
-        more solid understanding of how systems and applications work. mbitious
-        problem solver with a passion for Online businesses and would like to
-        join a team of like-minded developers. Constantly striving to improve
-        myself and enhance my skills in technologies.
+        more. A solid understanding of how systems and applications work.
+        mbitious problem solver with a passion for Online businesses and would
+        like to join a team of like-minded developers. Constantly striving to
+        improve myself and enhance my skills in technologies.
       </div>
     </div>
   );
