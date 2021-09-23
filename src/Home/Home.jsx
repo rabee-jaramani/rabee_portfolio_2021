feceb70ef9c15fef92e60dbdbaa32c2a227e347a
Rabee Aljaramani <77026472+rabeexxxx@users.noreply.github.com>
2021-09-23 13:37:10 +0400

    alien

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -6,13 +6,66 @@ import { useSelector } from "react-redux";
 import { Timeline } from "gsap/gsap-core";
 import Steve from "./Steve/Steve";
 import Moon from "./Moon";
-import gsap from "gsap/all";
-
+import gsap, { Sine } from "gsap/all";
+import alien from "./alien.png";
 const Home = () => {
   const season_img_ext = useSelector((state) => state.Season.season_img_ext);
   const season = useSelector((state) => state.Season);
 
   useEffect(() => {
+    // ////////////////////////////// alien floating
+
+    const alien = document.querySelector(".animation-cont");
+
+    const randomX = random(10, 40);
+    const randomY = random(40, 60);
+    const randomTime = random(3, 5);
+    const randomTime2 = random(5, 10);
+    const randomAngle = random(8, 12);
+
+    gsap.set(alien, {
+      x: randomX(-1),
+      y: randomX(1),
+      rotation: randomAngle(-1),
+    });
+
+    moveX(alien, 1);
+    moveY(alien, -1);
+    rotate(alien, 1);
+
+    function rotate(target, direction) {
+      gsap.to(target, randomTime2(), {
+        rotation: randomAngle(direction),
+        // delay: randomDelay(),
+        ease: Sine.easeInOut,
+        onComplete: rotate,
+        onCompleteParams: [target, direction * -1],
+      });
+    }
+
+    function moveX(target, direction) {
+      gsap.to(target, randomTime(), {
+        x: randomX(direction),
+        ease: Sine.easeInOut,
+        onComplete: moveX,
+        onCompleteParams: [target, direction * -1],
+      });
+    }
+
+    function moveY(target, direction) {
+      gsap.to(target, randomTime(), {
+        y: randomY(direction),
+        ease: Sine.easeInOut,
+        onComplete: moveY,
+        onCompleteParams: [target, direction * -1],
+      });
+    }
+
+    function random(min, max) {
+      const delta = max - min;
+      return (direction = 1) => (min + delta * Math.random()) * direction;
+    }
+    // ////////////////
     const TL = new Timeline();
     const duration = 0.3;
     var delay = 0.5;
@@ -65,6 +118,22 @@ const Home = () => {
       </div>
       <Moon />
       <Steve />
+      <div
+        className="animation-cont"
+        onClick={() => {
+          gsap.to(".animation-cont", { transform: "scale(0.1)", duration: 1 });
+          setTimeout(() => {
+            window
+              .open(
+                "https://github.com/rabeexxxx/rabee_portfolio_2021",
+                "_blank"
+              )
+              .focus();
+          }, 1000);
+        }}
+      >
+        <img src={alien} alt="" className="gif-img" />
+      </div>
     </div>
   );
 };
